6c78d88b6e501523e12a01defe738912231e6bad
Daniele Scarparo <daniele.scarparo@dextra-sw.com>
2020-05-25 17:23:01 -0300

    create delete product

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -23,9 +23,15 @@ module.exports = {
     },
     //update product
     async update(req, res) {
+        //new : true return product update
         const product = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true});
 
         return res.json(product);
     },
-
+    //delete product
+    async destroy(req, res) {
+        await Product.findByIdAndDelete(req.params.id);
+        //not return product, only status of delete
+        return res.send();
+    }
 };
