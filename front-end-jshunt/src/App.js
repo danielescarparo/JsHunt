b84f547e4eb7273398419f813ecafe990653b415
Daniele Scarparo <daniele.scarparo@dextra-sw.com>
2020-05-30 21:00:35 -0300

    adding route to product

--- front-end-jshunt/src/App.js
+++ front-end-jshunt/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/index'
-import Main from './pages/main'
+import Routes from './routes';
 
 const App = () => (
   <div className="App">
     <Header/>
-    <Main/>
+    <Routes/>
   </div>
 );
 
